387ee46b1d998b688edba2d7d5b18d024bc2e329
fernan23 <fdsoria23@gmail.com>
2023-09-25 19:30:45 -0300

    commit 2

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-do {
-    var cal = prompt("Ingrese su calificacion");
+function evaluarCalificaciones() {
+    do {
+      var cal = prompt("Ingrese su calificacion");
   
-    if (cal === null) {
-      break;
-    }
+      if (cal === null) {
+        break; 
+      }
   
-    cal = parseFloat(cal); 
+      cal = parseFloat(cal);
   
-    if (!isNaN(cal)){ 
-      if (cal >= 0 && cal <= 10) {
-        if (cal < 3) {
-          alert("Recursar");
-        } else if (cal < 5) {
-          alert("Recupera");
-        } else if (cal < 8) {
-          alert("Suficiente, Aprueba");
-        } else if (cal < 9) {
-          alert("Sobresaliente, Aprueba");
+      if (!isNaN(cal)) { 
+        if (cal >= 0 && cal <= 10) {
+          if (cal < 3) {
+            alert("Recursar");
+          } else if (cal < 5) {
+            alert("Recupera");
+          } else if (cal < 8) {
+            alert("Suficiente, Aprueba");
+          } else if (cal < 9) {
+            alert("Sobresaliente, Aprueba");
+          } else {
+            alert("Excelente, Aprueba");
+          }
         } else {
-          alert("Excelente, Aprueba");
+          alert("Error: Ingrese una cualificacion dentro del rango 0-10");
         }
       } else {
-        alert("Error: Ingrese un calificacion dentro del rango 0-10");
+        alert("Error: Ingrese u valor numérico válido");
       }
-    } else {
-      alert("Error: Ingrese un valor numerico válido");
-    }
-  } while (true);
+    } while (true);
+  }
+  
+  evaluarCalificaciones();
   
